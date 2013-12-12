8e42199f56058dd21c21a983dc1a51dd9a6a84ba
askos92 <askoanti@gmail.com>
2013-12-12 07:57:24 +0200

    Added Attributes class and list of attributes in products

--- QorderWS/src/main/java/com/qorder/qorderws/model/product/Product.java
+++ QorderWS/src/main/java/com/qorder/qorderws/model/product/Product.java
@@ -1,12 +1,23 @@
 package com.qorder.qorderws.model.product;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import com.qorder.qorderws.model.attribute.Attribute;
 
 @Entity
 @Table(name="PRODUCT")
@@ -20,6 +31,11 @@ public class Product {
 	private String name;
 	@Column(name="PRICE")
 	private BigDecimal price;
+	
+	@OneToMany(targetEntity = Attribute.class, cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+	@Fetch(value = FetchMode.SUBSELECT)
+	@JoinColumn(name = "PRODUCT_ID")
+	private List<Attribute> AttributeList = new ArrayList<Attribute>();
 	
 	
 	public Product(String name, BigDecimal price) {
@@ -54,5 +70,15 @@ public class Product {
 	public void setPrice(BigDecimal price) {
 		this.price = price;
 	}
+
+	public List<Attribute> getAttributeList() {
+		return AttributeList;
+	}
+
+	public void setAttributeList(List<Attribute> attributeList) {
+		AttributeList = attributeList;
+	}
+	
+	
 
 }
