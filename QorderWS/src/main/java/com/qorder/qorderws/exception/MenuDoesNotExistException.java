f1684380c62886a84c39fc1e56c735fb5f519b42
gregoavg <grigoris.avgitidis@gmail.com>
2014-02-04 17:38:56 +0200

    Fixed: Testing configuration Upgraded: JUinit Test libraries to ver 4.11 Upgraded: Spring framework to new ver 4

--- QorderWS/src/main/java/com/qorder/qorderws/exception/MenuDoesNotExistException.java
+++ QorderWS/src/main/java/com/qorder/qorderws/exception/MenuDoesNotExistException.java
@@ -2,6 +2,11 @@ package com.qorder.qorderws.exception;
 
 public class MenuDoesNotExistException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public MenuDoesNotExistException() {
 		super();
 	}
--- QorderWS/src/main/java/com/qorder/qorderws/exception/ProductDoesNotExistException.java
+++ QorderWS/src/main/java/com/qorder/qorderws/exception/ProductDoesNotExistException.java
@@ -1,6 +1,11 @@
 package com.qorder.qorderws.exception;
 
 public class ProductDoesNotExistException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8529001365403266239L;
+
 	public ProductDoesNotExistException() {
 		super();
 	}
