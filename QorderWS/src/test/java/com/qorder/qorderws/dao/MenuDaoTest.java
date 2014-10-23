316459a848cd1715fa22ae9bdcc0ce1e70756d76
gregoavg <grigoris.avgitidis@gmail.com>
2014-10-24 02:20:13 +0300

    Fixed: MenuDAO test-case initialization

--- QorderWS/src/test/java/com/qorder/qorderws/dao/MenuDaoTest.java
+++ QorderWS/src/test/java/com/qorder/qorderws/dao/MenuDaoTest.java
@@ -10,13 +10,18 @@ import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.dbunit.operation.DatabaseOperation;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.qorder.qorderws.exception.ResourceNotFoundException;
+import com.qorder.qorderws.model.menu.Menu;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/test-context.xml" })
@@ -41,8 +46,15 @@ public class MenuDaoTest extends DBTestCase {
 		DatabaseOperation.CLEAN_INSERT.execute(connection, getDataSet());
 	}
 	
+	@Test
+	public void testExistsFindById() {
+		Menu menu = testMenuDAO.findById(4);
+		Assert.assertNotNull(menu);
+	}
 	
-	
-	
-
+	@Test(expected = ResourceNotFoundException.class)
+	public void testMenuNotFoundByID() {
+		Menu aMenu = testMenuDAO.findById(2500);
+		Assert.assertNull(aMenu);
+	}
 }
